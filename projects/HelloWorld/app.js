1e1ed72532f9deacaa836b8cf2b3075276fc6d3e
arseitz <7088383+arseitz@users.noreply.github.com>
2018-12-14 15:17:00 -0600

    Update app.js
    Removed OOP stuff

--- projects/HelloWorld/app.js
+++ projects/HelloWorld/app.js
@@ -48,8 +48,6 @@ Ext.application({
         
         this.testSomeUI();
         
-        this.testSomeObjects();
-        
     },
     
     // --------------------------------------
@@ -67,78 +65,6 @@ Ext.application({
                 }
             ]
         });
-    
-    },
-    
-    // --------------------------------------
-    // Methods
-    // --------------------------------------
-    testSomeObjects: function() 
-    {
-
-		// ****************
-        // SUPER CLASS
-		// ****************
-		console.log ("\nSUPER CLASS");
-        var myBeer = Ext.create('com.rmc.projects.helloworld.Beer', 'Budweiser');
-        myBeer.addListener ('drinked', function () {
-            var stringArgument = arguments[0];
-            console.log (myBeer.brandName + " was drinked! With message" + stringArgument + ".");
-        });
-        console.log ("myBeer.brandName: " + myBeer.brandName );    
-        myBeer.drink();
-
-
-		// ****************
-        // CHILD CLASS
-		// ****************
-		console.log ("\nCHILD CLASS");
-        var myLightBeer = Ext.create('com.rmc.projects.helloworld.LightBeer', 'BudLight');
-        myLightBeer.addListener ('drinked', function () {
-            var stringArgument = arguments[0];
-            console.log (myLightBeer.brandName + " was drinked! With message" + stringArgument + ".");
-        });
-        console.log ("myLightBeer.brandName: " +myLightBeer.brandName );
-        myLightBeer.drink();
-        
-        
-		// ****************
-        // MIXIN
-		// ****************
-		console.log ("\nMIXIN");
-        //  Because of 'MixinCheers.js' we can call 'cheers()'
-        myLightBeer.cheers();
-        
-
-        
-		// ****************
-        // OBJECT ORIENTED DEMO
-        //
-        //	NOTE: This shows some of the concepts above, and more.
-        //
-		// ****************
-		//	CREATE & PASS PARAMETER TO CONSTUCTOR
-		console.log ("\nOBJECT ORIENTED DEMO");
-        var cocktail = Ext.create('com.rmc.projects.helloworld.Cocktail', 'RedBullAndVodka');
-        
-		//	TEST PUBLIC PROPERTY
-		console.log ("cocktail.samplePublicProperty : " + cocktail.samplePublicProperty );
-		
-		//	TEST STATIC PROPERTY
-		console.log ("com.rmc.projects.helloworld.Cocktail.TEST_STATIC_PROPERTY : " + com.rmc.projects.helloworld.Cocktail.TEST_STATIC_PROPERTY );
-		
-		//	TEST AUTO-GENERATED GETTER/SETTER
-		console.log ("cocktail.getCountForFun() : " + cocktail.getCountForFun() );
-		
-		//	TEST LISTENER
-        cocktail.addListener ('customEventCalled', function () {
-			console.log ("cocktail.customEventCalled argument[0] : " + arguments[0] );
-        });
-        cocktail.customEventCall();
-
-        
-        
-        
     
     }
     
